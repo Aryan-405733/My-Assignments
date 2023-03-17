2bc0d7d07581b2a4d2cc91b8d2a8cb098a87a7de
Aryan Sharma <as405733@gmail.com>
2023-03-17 16:59:24 +0530

    Updated Files

---
+++ 17-03-23/userInput/rest.js
@@ -0,0 +1,9 @@
+function display(a,b,c){
+    console.log(a,b,c);
+}
+function display_adv(a,b,...c){
+    console.log(a,b,c);
+
+}
+display(1,2,3,4,5); // 1,2,3 rest are dropped
+display_adv(1,2,3,5);// 1,2,[3,4,5]// rest parameters.
